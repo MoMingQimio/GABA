a128c646a313588602f3506e924dffbd1129593f
Haocheng Xu <247698454@qq.com>
2024-11-06 12:00:54 +0800

    1

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-import gym
+
 import os
-import glob
-import time
 from datetime import datetime
 from itertools import count
 import torch
 import numpy as np
 import math
 import gym
-# from networkx.conftest import collect_ignore
-
-# from sympy.abc import epsilon
+from multiprocessing import Process
 
 from PPO import PPO
 from dqn_sumo_gym import Agent
 
 
-# from gym_sumo.envs.RuleBasedDriverModel import Vehicle
 
 
-################################### Training ###################################
+
+
 def train(seed = 0):
     print("程序正在运行中，请勿关闭电脑")
     # set device to cpu or cuda
@@ -30,31 +26,14 @@ def train(seed = 0):
         print("Device set to : " + str(torch.cuda.get_device_name(device)))
     else:
         print("Device set to : cpu")
-    # print("============================================================================================")
-    #
-    # print("============================================================================================")
 
     ####### initialize environment hyperparameters ######
     env_name = "sumo-v0"
-
     has_continuous_action_space = False  # continuous action space; else discrete
-
-    max_ep_len = 1000  # max timesteps in one episode
     max_training_timesteps = int(3e6)  # break training loop if timeteps > max_training_timesteps
-
-    print_freq = 10  # print avg reward in the interval (in num timesteps)
-    log_freq = 1  # log avg reward in the interval (in num timesteps)
     save_model_freq = int(1e5)  # save model frequency (in num timesteps)
-
     action_std = 0.6  # starting std for action distribution (Multivariate Normal)
-    action_std_decay_rate = 0.05  # linearly decay action_std (action_std = action_std - action_std_decay_rate)
-    min_action_std = 0.1  # minimum action_std (stop decay after action_std <= min_action_std)
-    action_std_decay_freq = int(2.5e5)  # action_std decay frequency (in num timesteps)
-    #####################################################
 
-    ## Note : print/log frequencies should be > than max_ep_len
-
-    ################ PPO hyperparameters ################
     update_timestep = 1000 # update policy every n timesteps
     K_epochs = 80  # update policy for K epochs in one PPO update
 
@@ -65,15 +44,10 @@ def train(seed = 0):
     lr_critic = 0.001  # learning rate for critic network
 
     random_seed = int(seed)  # set random seed if required (0 = no random seed)
-    #####################################################
-
-    # print("training environment name : " + env_name)
 
     env = gym.make(env_name, render_mode="")
-
     # state space dimension
     state_dim = env.observation_space.shape[0]
-
     # action space dimension
     if has_continuous_action_space:
         action_dim = env.action_space.shape[0]
@@ -116,11 +90,7 @@ def train(seed = 0):
 
     av_log_f_name = av_dir + "DQN_" + env_name + "_log_" + str(av_run_num) +"_"+seed+ ".csv"
 
-    # print("current logging run number for " + env_name + " : ", run_num)
-    # print("logging at : " + log_f_name)
-    #####################################################
 
-    ################### checkpointing ###################
     run_num_pretrained = 0  #### change this to prevent overwriting weights in same env_name folder
 
     directory = "PPO_preTrained"
@@ -143,49 +113,12 @@ def train(seed = 0):
     av_checkpoint_path = directory + "AV_{}_{}_{}.pth".format(env_name, random_seed, run_num_pretrained)
 
     print("save checkpoint path : " + checkpoint_path)
-    #####################################################
 
-    # ############# print all hyperparameters #############
-    # print("--------------------------------------------------------------------------------------------")
-    # print("max training timesteps : ", max_training_timesteps)
-    # print("max timesteps per episode : ", max_ep_len)
-    # print("model saving frequency : " + str(save_model_freq) + " timesteps")
-    # print("log frequency : " + str(log_freq) + " timesteps")
-    # print("printing average reward over episodes in last : " + str(print_freq) + " timesteps")
-    # print("--------------------------------------------------------------------------------------------")
-    # print("state space dimension : ", state_dim)
-    # print("action space dimension : ", action_dim)
-    # print("--------------------------------------------------------------------------------------------")
-    # if has_continuous_action_space:
-    #     print("Initializing a continuous action space policy")
-    #     print("--------------------------------------------------------------------------------------------")
-    #     print("starting std of action distribution : ", action_std)
-    #     print("decay rate of std of action distribution : ", action_std_decay_rate)
-    #     print("minimum std of action distribution : ", min_action_std)
-    #     print("decay frequency of std of action distribution : " + str(action_std_decay_freq) + " timesteps")
-    # else:
-    #     print("Initializing a discrete action space policy")
-    # print("--------------------------------------------------------------------------------------------")
-    # print("PPO update frequency : " + str(update_timestep) + " timesteps")
-    # print("PPO K epochs : ", K_epochs)
-    # print("PPO epsilon clip : ", eps_clip)
-    # print("discount factor (gamma) : ", gamma)
-    # print("--------------------------------------------------------------------------------------------")
-    # print("optimizer learning rate actor : ", lr_actor)
-    # print("optimizer learning rate critic : ", lr_critic)
     if random_seed:
-        print("--------------------------------------------------------------------------------------------")
-        print("setting random seed to ", random_seed)
         torch.manual_seed(random_seed)
         env.seed(random_seed)
         np.random.seed(random_seed)
-    #####################################################
 
-    # print("============================================================================================")
-
-    ################# training procedure ################
-
-    # initialize a PPO agent
     ppo_agent = PPO(state_dim + 1, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip,
                     has_continuous_action_space, action_std)
     AV_agent = Agent("Agent", env.action_space.nvec[1], state_dim + 1)
@@ -304,63 +237,21 @@ def train(seed = 0):
                 current_ep_reward += BV_reward
                 if (adversarial_counts+1) % update_timestep == 0:
                     ppo_agent.update()
-                # update PPO agent
-                # if (adversarial_counts + 1) % update_timestep == 0:
-                
-
-                # if continuous action space; then decay action std of ouput action distribution
-                # if has_continuous_action_space and adversarial_counts % action_std_decay_freq == 0:
-                #     ppo_agent.decay_action_std(action_std_decay_rate, min_action_std)
 
             if collision_flag:
-                print("Collision detected")
+                #print("Collision detected")
                 collision_counts += 1
-                
-                # log in logging file
-                # 需要修改
-                # if (collision_counts + 1) % log_freq == 0:
-                # log average reward till last episode4s
-                # log_avg_reward = log_running_reward / log_running_episodes
                 current_ep_reward = round(current_ep_reward, 4)
-                
-
                 log_f.write('{},{},{},{},{}\n'.format(i_episode, time_step, collision_counts, adversarial_counts,
                                                       current_ep_reward))
                 # print("BV--> Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, time_step,
                 #                                                                               log_avg_reward))
                 log_f.flush()
-
-                # log_running_reward = 0
-                # log_running_episodes = 1
-
-                # printing average reward
-                # if (collision_counts + 1) % print_freq == 0:
-                #     # print average reward till last episode
-                #     print_avg_reward = print_running_reward / print_running_episodes
-                #     print_avg_reward = round(print_avg_reward, 2)
-                #
-                #     print("Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, time_step,
-                #                                                                             print_avg_reward))
-                #
-                #     print_running_reward = 0
-                #     print_running_episodes = 0
-
-                # save model weights
                 if (collision_counts + 1) % save_model_freq == 0:
-                    # print(
-                    #     "--------------------------------------------------------------------------------------------")
-                    # print("saving model at : " + checkpoint_path)
                     ppo_agent.save(checkpoint_path)
-                    # print("model saved")
-                    # print("Elapsed Time  : ", datetime.now().replace(microsecond=0) - start_time)
-                    # print(
-                    #     "--------------------------------------------------------------------------------------------")
 
-            # excepted_risk_last_time = excepted_risk
 
             r_r += reward
-            # if reward == -10:
-            #     print(f'Collision: {reward}')
             reward = torch.tensor([reward], device=device)
 
             # done = terminated
@@ -376,19 +267,18 @@ def train(seed = 0):
             AV_agent.updateTargetNetwork()
 
             if done:
-                # AV_agent.episode_durations.append(r_r)
-                # print(done)
+
                 av_running_time = env.getRunningTime()
                 av_distance = env.getDistance()
 
-                # AV_agent.plot_durations()
+
                 env.closeEnvConnection()
-                # print(f'Episodes:{i_episode + 1}, Reward: {r_r}')
+
                 break
 
             env.move_gui()
 
-            # print(f'epsilon:{epsilon}')
+
 
         collision_rate = collision_counts / (i_episode + 1)
         # epsilon = 1 / (1+np.exp(-1*(2 - collision_rate - (total_risk + 1e-3)/2)))
@@ -408,15 +298,6 @@ def train(seed = 0):
         av_right_avg = np.sum(av_right_change) / (t + 1)
         av_total_risk_avg = np.mean(av_total_risk)
 
-        # av_speed_avg = round(av_speed_avg, 4)
-        # av_total_risk_avg = round(av_total_risk_avg, 4)
-        # av_distance = round(av_distance, 4)
-        # av_running_time = round(av_running_time, 4)
-        # av_acc_avg = round(av_acc_avg, 4)
-        # av_dece_avg = round(av_dece_avg, 4)
-        # av_left_avg = round(av_left_avg, 4)
-        # av_right_avg = round(av_right_avg, 4)
-
         av_log_f.write('{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(i_episode, time_step, r_r, if_collision,
                                                                                 collision_rate, adversarial_counts,
                                                                                 epsilon, av_speed_avg,
@@ -433,37 +314,23 @@ def train(seed = 0):
 
         i_episode += 1
 
-        # print_running_reward += current_ep_reward
-        # print_running_episodes += 1
-
-        # log_running_reward += current_ep_reward
-        # log_running_episodes += 1
 
     log_f.close()
     av_log_f.close()
-    # env.closeEnvConnection()
-
-    # print total training time
-    # print("============================================================================================")
-    # end_time = datetime.now().replace(microsecond=0)
-    # print("Started training at (GMT) : ", start_time)
-    # print("Finished training at (GMT) : ", end_time)
-    # print("Total training time  : ", end_time - start_time)
-    # print("============================================================================================")
 
 
-# if __name__ == '__main__':
-#     train()
+    #print total training time
+    print("============================================================================================")
+    end_time = datetime.now().replace(microsecond=0)
+    print("Started training at (GMT) : ", start_time)
+    print("Finished training at (GMT) : ", end_time)
+    print("Total training time  : ", end_time - start_time)
+    print("============================================================================================")
 
 
-# def mul_proc_training(process):
-# 	agent = DQNPERAgent(process)
-# 	#agent = DQNAgent(process)
-# 	env = gym.make("sumo-v0", seed="12454"+str(process), render_mode="")
-# 	agent.train_RL(env) # for training
 
-import multiprocessing as mp
-from multiprocessing import Pool, Process
+
+
 def main():
 	ra = [i for i in range(3)]
 	proces = []
